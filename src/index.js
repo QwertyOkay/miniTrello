e058cd8d336297750f8c112b075dcd0023038326
Alexandr Kotenko <kpshmt@gmail.com>
2024-02-12 03:18:03 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './components/App';
+import client from './apolloClient';
+import { ApolloProvider } from '@apollo/client';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const container = document.getElementById('root');
+const root = createRoot(container); 
+
 root.render(
+  <ApolloProvider client={client}>
     <App />
+  </ApolloProvider>
 );
